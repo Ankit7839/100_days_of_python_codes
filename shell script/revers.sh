a93e785ca545ca63bc61db7f66fc24633e419907
Ankit7839 <36545434+Ankit7839@users.noreply.github.com>
2018-08-02 00:57:36 +0530

    for revers number

--- shell script/revers.sh
+++ shell script/revers.sh
@@ -3,8 +3,9 @@ read number
 sum=0
 while [ $number - 0 ]
   do
-    rem= `expr $number \% 10`
-    sum= `expr $sum \* 10 \+ $rem`
-    number= `expr $number \/10`
+    rem= `expr $number % 10`
+    number= `expr $number / 10`
+    sum= `expr $sum \* 10 + $rem`
+    
   done
 echo "reversed number is $sum"
