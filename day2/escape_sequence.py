048d722e707d7d6cd8d3808d60bf9f2db8e6e76b
Ankit7839 <36545434+Ankit7839@users.noreply.github.com>
2018-08-05 12:25:56 +0530

    escape_sequence use in python

---
+++ day2/escape_sequence.py
@@ -0,0 +1,8 @@
+#this is double backslash
+print("this is \\\\ double backslash ")
+# these are /\/\/\/\/\ mountains
+print("these are '/\\/\\/\\/\\/\\' mountain")
+#this is    awesome
+print("this is \tawesome")
+#\" \n \t \' as out put
+print("\\\" \\n \\t  \\\'")
