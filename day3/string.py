1903ed01c26bc37fb8165239fc7c499a65e105fa
Ankit7839 <36545434+Ankit7839@users.noreply.github.com>
2018-08-06 22:00:17 +0530

    for string in python

---
+++ day3/string.py
@@ -0,0 +1,10 @@
+first_name = "ANKIT "
+last_name = "ASTHANA"
+full_name = first_name + last_name
+print(full_name)
+#print(first_name + 3) error
+print(first_name + "3")
+#or
+print(first_name + str(3))#no error
+#or
+print(first_name * 3)
