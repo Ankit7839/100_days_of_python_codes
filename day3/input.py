72552d707776f7ef762c46eae85dd5131c8e93e7
Ankit7839 <36545434+Ankit7839@users.noreply.github.com>
2018-08-06 22:41:00 +0530

    input from users

---
+++ day3/input.py
@@ -0,0 +1,7 @@
+#how to get input from users
+Name=input("Enter your name=")
+print("Hello " + Name)
+AGE=input("Enter your age=")
+print("your age " + str(AGE))#no error
+#or
+print("your age "+ AGE)
