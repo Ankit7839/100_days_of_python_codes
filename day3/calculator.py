ccb2a56e190add84bd307d25421ea7cac81f70cd
Ankit7839 <36545434+Ankit7839@users.noreply.github.com>
2018-08-06 21:01:04 +0530

    some calculation

---
+++ day3/calculator.py
@@ -0,0 +1,8 @@
+print(2/4) #float div.
+print(4/2) #flaat div.
+print(4//2) #integer div.
+print(2//4) #integer div
+print(2**3) #exponent
+print(2**0.5)#squerroot
+print(2+3*4)#expresion
+print(round(2**0.5,4))#round off 
