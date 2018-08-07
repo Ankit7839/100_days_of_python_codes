7f6437905a2af3d6e0bda0676e9ec7a7e3332ad4
Ankit7839 <36545434+Ankit7839@users.noreply.github.com>
2018-08-07 11:53:34 +0530

    day 4 for sum and string formetting

---
+++ day4/string_formeting.py
@@ -0,0 +1,12 @@
+Name = "Ankit "
+Age = 23
+print("Hello " + Name + "your age is " + str(Age))# ugly syntax
+#string formatting
+
+print("Hello {} your age is {} ".format(Name, Age))
+#or
+print(f"Hello {Name} your age is {Age}")
+#out put is 
+# Hello Ankit your age is 23
+# Hello Ankit your age is 23
+# Hello Ankit your age is 23
---
+++ day4/sum.py
@@ -0,0 +1,16 @@
+#for integer value
+print("Enter interger value ")
+num_one = int(input("Enter the first element="))
+num_two = int(input("Enter the second element="))
+Add = int(num_one + num_two)
+print("ADD is= " + str (Add))
+#output is
+# x + y = z
+# for float value
+print("enter float value")
+num_one = float(input("Enter the first element="))
+num_two = float(input("Enter the second element="))
+Add = float(num_one + num_two)
+print(Add)
+
+
