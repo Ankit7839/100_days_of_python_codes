2a8ebada9873f4151b0451c845a600be14b0d5a7
Ankit7839 <36545434+Ankit7839@users.noreply.github.com>
2018-08-08 22:20:01 +0530

    average of 3 numbers

---
+++ day5/average.py
@@ -0,0 +1,10 @@
+num1 = input("Enter first number :")
+num2 = input("Enter second number :")
+num3 = input("Enter third number :")
+print(f"average of three number : {(int(num1) + int(num2) +int(num3)) / 3}")
+
+#out put
+#Enter first number :4
+#Enter second number :8
+#Enter third number :6
+#average of three number : 6.0
