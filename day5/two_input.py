f14062767790368978d4587b950a8ceed9c7f232
Ankit7839 <36545434+Ankit7839@users.noreply.github.com>
2018-08-08 21:53:41 +0530

    two_input.py

---
+++ day5/two_input.py
@@ -0,0 +1,10 @@
+# name = input("Enter your name :")
+# age = input("Enter you age :")
+name, age = input("Enter your name and age  ").split(",")
+print(name)
+print(age)
+
+#output
+#Enter your name and age  Ankit,23
+#Ankit
+#23
