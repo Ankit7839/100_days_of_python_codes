d7a82a77aac7b8f6bde9c556c4db242c661fe265
Ankit7839 <36545434+Ankit7839@users.noreply.github.com>
2018-08-09 20:24:49 +0530

    for slicing in python

---
+++ day6/string_slicing.py
@@ -0,0 +1,33 @@
+#slicing / selecting sub sequences
+lang = "python"
+#syntex -[start argument : stop argument -1]
+print(lang[0:6])
+print(lang[0:5])
+print(lang[0:4])
+print(lang[0:3])
+print(lang[0:2])
+print(lang[0:1])
+print(lang[0:-1])
+print(lang[0:-2])
+print(lang[0:-3])
+print(lang[:])
+print(lang[1:])
+print(lang[:3])
+
+#output is 
+#python
+#pytho
+#pyth
+#pyt
+#py
+#p
+#pytho
+#pyth
+#pyt
+#python
+#ython
+#pyt
+
+
+
+
