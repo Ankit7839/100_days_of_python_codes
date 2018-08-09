ed8a219af402673fb54243a9feb9e13127f92e64
Ankit7839 <36545434+Ankit7839@users.noreply.github.com>
2018-08-09 20:15:15 +0530

    string_indexing

---
+++ day6/string_indexing.py
@@ -0,0 +1,20 @@
+#string indexing
+language = "ANKIT ASTHANA"
+print(language[3])
+print(language[-1])
+print(language[+1])
+print(language[5])
+#print(language[14])
+
+#error in 7 line
+#Traceback (most recent call last):
+ # File "string_indexing.py", line 7, in <module>
+  #  print(language[14])
+#IndexError: string index out of range
+print(language[12])
+#out put is
+#I
+#A
+#N
+
+#A
