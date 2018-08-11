43e05ddb29182c087017d878753f979fd74fa45a
Ankit7839 <36545434+Ankit7839@users.noreply.github.com>
2018-08-11 18:26:25 +0530

    for step_argument

---
+++ day8/step_argument.py
@@ -0,0 +1,18 @@
+#step_argumen sub secuences
+#synatx -[start argument : stop argument -1 : step]
+print("Ankit"[0:3:2])
+print("ANKIT"[0:4:2])
+print("ANKIT"[0:2:1])
+print("ANKIT"[::1])
+print("ANKIT"[::4]) 
+print("ANKIT"[::-1])#trick
+
+
+#out put is 
+#Ak
+#AK
+#AN
+#ANKIT
+#AT
+#TIKNA
+
